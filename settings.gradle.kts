c45082a76ffa0a62aa2c22c2f33e2c1aeafbd2d6
julio7173 <juliocesarsevericheorellana@gmail.com>
2025-05-09 16:52:24 -0400

    Update settings.gradle.lts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "limbus_server"
+
+include(":shared")
+include(":services:api-gateway")
+include(":services:auth-service")
+include(":services:health-professional-service")
+include(":services:meal-planning-service")
+include(":services:notification-service")
+include(":services:nutritional-service")
+include(":services:user-profile-service")
